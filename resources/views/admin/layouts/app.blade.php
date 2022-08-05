5148584ae6adaa9c04f6916b6617dc57645f9b4c
Yassine Bess <yazo-yazo@hotmail.com>
2022-08-05 15:59:39 +0100

    up

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -9,20 +9,20 @@
         <meta content="Coderthemes" name="author" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
 
         <!-- Plugins css -->
-        <link href="assets/libs/flatpickr/flatpickr.min.css" rel="stylesheet" type="text/css" />
-        <link href="assets/libs/selectize/css/selectize.bootstrap3.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset ('assets/libs/flatpickr/flatpickr.min.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{asset ('assets/libs/selectize/css/selectize.bootstrap3.css') }}" rel="stylesheet" type="text/css" />
 
         <!-- Bootstrap css -->
-        <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="{{asset ('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
         <!-- App css -->
-        <link href="assets/css/app.min.css" rel="stylesheet" type="text/css" id="app-style"/>
+        <link href="{{asset ('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" id="app-style"/>
         <!-- icons -->
-        <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
         <!-- Head js -->
-        <script src="assets/js/head.js"></script>
+        <script src="{{ asset('assets/js/head.js') }}"></script>
 
     </head>
 
@@ -34,19 +34,19 @@
         @include('admin.layouts.menu')
         @yield('content')
         <!-- Vendor js -->
-        <script src="assets/js/vendor.min.js"></script>
+        <script src="{{asset ('assets/js/vendor.min.js') }}"></script>
 
         <!-- Plugins js-->
-        <script src="assets/libs/flatpickr/flatpickr.min.js"></script>
-        <script src="assets/libs/apexcharts/apexcharts.min.js"></script>
+        <script src="{{asset ('assets/libs/flatpickr/flatpickr.min.js') }}"></script>
+        <script src="{{asset ('assets/libs/apexcharts/apexcharts.min.js') }}"></script>
 
-        <script src="assets/libs/selectize/js/standalone/selectize.min.js"></script>
+        <script src="{{asset ('assets/libs/selectize/js/standalone/selectize.min.js') }}"></script>
 
         <!-- Dashboar 1 init js-->
-        <script src="assets/js/pages/dashboard-1.init.js"></script>
+        <script src="{{asset ('assets/js/pages/dashboard-1.init.js') }}"></script>
 
         <!-- App js-->
-        <script src="assets/js/app.min.js"></script>
+        <script src="{{asset ('assets/js/app.min.js') }}"></script>
 
     </body>
 
