19066a6decd3c9fdff9fa9058caa4b8fb668b8b0
Yassine Bess <yazo-yazo@hotmail.com>
2022-08-09 15:35:58 +0100

    up

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -1,50 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=utf-8" /><!-- /Added by HTTrack -->
 <head>
-        <meta charset="utf-8" />
-        <title>@yield('title') | {{ config('app.name') }}</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
-        <meta content="" name="author" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Dlab - Event Admin Dashboard</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
+    <link href="{{asset ('admin/css/style.css') }}" rel="stylesheet">
 
-        <!-- App favicon -->
-        <link rel="shortcut icon" href="assets/images/favicon.ico">
+</head>
 
-        <!-- jvectormap -->
-        <link href="../plugins/jvectormap/jquery-jvectormap-2.0.2.css" rel="stylesheet">
+<body>
 
-        <!-- App css -->
-        <link href="{{ asset('admin/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('admin/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('admin/css/metisMenu.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="../plugins/daterangepicker/daterangepicker.css" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('admin/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+@include('admin.layouts.top_menu')
+@include('admin/layouts.side_bar')
 
-    </head>
+@yield('content')
 
-  <body>
-    @include('admin.layouts.top_menu')
-    @include('admin.layouts.menu')
-    @yield('content')
-        <!-- jQuery  -->
-        <script src="{{ asset('admin/js/jquery.min.js') }}"></script>
-        <script src="{{ asset('admin/js/bootstrap.bundle.min.js') }}"></script>
-        <script src="{{ asset('admin/js/metismenu.min.js') }}"></script>
-        <script src="{{ asset('admin/js/waves.js') }}"></script>
-        <script src="{{ asset('admin/js/feather.min.js') }}"></script>
-        <script src="{{ asset('admin/js/simplebar.min.js') }}"></script>
-        <script src="{{ asset('admin/js/moment.js') }}"></script>
-        <script src="../plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="{{asset ('admin/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{asset ('admin/vendor/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{asset ('admin/vendor/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+    <!-- Here is navigation script -->
+    <script src="{{ asset('admin/vendor/quixnav/quixnav.min.js') }}"></script>
+    <script src="{{asset ('admin/js/quixnav-init.js') }}"></script>
+    <script src="{{ asset('admin/js/custom.min.js') }}"></script>
+    <!--removeIf(production)-->
+    <!-- Demo scripts -->
+    <script src="{{ asset('admin/js/styleSwitcher.js') }}"></script>
+    <!--endRemoveIf(production)-->
 
-        <script src="../plugins/apex-charts/apexcharts.min.js"></script>
-        <script src="../plugins/jvectormap/jquery-jvectormap-2.0.2.min.js"></script>
-        <script src="../plugins/jvectormap/jquery-jvectormap-us-aea-en.js"></script>
-        <script src="{{ asset('admin/pages/jquery.analytics_dashboard.init.js') }}"></script>
+    <!-- Daterange picker library -->
+    <script src="{{ asset('admin/vendor/circle-progress/circle-progress.min.js') }}"></script>
 
-        <!-- App js -->
-        <script src="{{ asset('admin/js/app.js') }}"></script>
+    <script src="{{asset ('admin/vendor/chart.js/Chart.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin/js/dashboard/dashboard-2.js') }}"></script>
 
-    </body>
+
+</body>
+
+
 </html>
--- resources/views/admin/layouts/top_menu.blade.php
+++ resources/views/admin/layouts/top_menu.blade.php
@@ -1,121 +1,108 @@
-   <!-- Top Bar Start -->
-   <div class="topbar">
-    <!-- Navbar -->
-    <nav class="navbar-custom">
-        <ul class="list-unstyled topbar-nav float-end mb-0">
-            <li class="dropdown hide-phone">
-                <a class="nav-link dropdown-toggle arrow-none waves-light waves-effect" data-bs-toggle="dropdown" href="#" role="button"
-                    aria-haspopup="false" aria-expanded="false">
-                    <i data-feather="search" class="topbar-icon"></i>
-                </a>
 
-                <div class="dropdown-menu dropdown-menu-end dropdown-lg p-0">
-                    <!-- Top Search Bar -->
-                    <div class="app-search-topbar">
-                        <form action="#" method="get">
-                            <input type="search" name="search" class="from-control top-search mb-0" placeholder="Type text...">
-                            <button type="submit"><i class="ti-search"></i></button>
+<div class="header">
+    <div class="header-content">
+        <nav class="navbar navbar-expand">
+            <div class="header-left">
+                <div class="nav-item dropdown search_bar">
+                    <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="mdi mdi-magnify"></i>
+                    </a>
+                    <div class="dropdown-menu">
+                        <form class="form-inline">
+                            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
                         </form>
                     </div>
                 </div>
-            </li>
+            </div>
+            <div class="collapse navbar-collapse justify-content-end">
 
-            <li class="dropdown notification-list">
-                <a class="nav-link dropdown-toggle arrow-none waves-light waves-effect" data-bs-toggle="dropdown" href="#" role="button"
-                    aria-haspopup="false" aria-expanded="false">
-                    <i data-feather="bell" class="align-self-center topbar-icon"></i>
-                    <span class="badge bg-danger rounded-pill noti-icon-badge">2</span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-end dropdown-lg pt-0">
-
-                    <h6 class="dropdown-item-text font-15 m-0 py-3 border-bottom d-flex justify-content-between align-items-center">
-                        Notifications <span class="badge bg-primary rounded-pill">2</span>
-                    </h6>
-                    <div class="notification-menu" data-simplebar>
-                        <!-- item-->
-                        <a href="#" class="dropdown-item py-3">
-                            <small class="float-end text-muted ps-2">2 min ago</small>
-                            <div class="media">
-                                <div class="avatar-md bg-soft-primary">
-                                    <i data-feather="shopping-cart" class="align-self-center icon-xs"></i>
-                                </div>
-                                <div class="media-body align-self-center ms-2 text-truncate">
-                                    <h6 class="my-0 fw-normal text-dark">Your order is placed</h6>
-                                    <small class="text-muted mb-0">Dummy text of the printing and industry.</small>
-                                </div><!--end media-body-->
-                            </div><!--end media-->
-                        </a><!--end-item-->
-                        <!-- item-->
-                        <a href="#" class="dropdown-item py-3">
-                            <small class="float-end text-muted ps-2">10 min ago</small>
-                            <div class="media">
-                                <div class="avatar-md bg-soft-primary">
-                                    <img src="assets/images/users/user-4.jpg" alt="" class="thumb-sm rounded-circle">
-                                </div>
-                                <div class="media-body align-self-center ms-2 text-truncate">
-                                    <h6 class="my-0 fw-normal text-dark">Meeting with designers</h6>
-                                    <small class="text-muted mb-0">It is a long established fact that a reader.</small>
-                                </div><!--end media-body-->
-                            </div><!--end media-->
-                        </a><!--end-item-->
-                        <!-- item-->
-                        <a href="#" class="dropdown-item py-3">
-                            <small class="float-end text-muted ps-2">40 min ago</small>
-                            <div class="media">
-                                <div class="avatar-md bg-soft-primary">
-                                    <i data-feather="users" class="align-self-center icon-xs"></i>
-                                </div>
-                                <div class="media-body align-self-center ms-2 text-truncate">
-                                    <h6 class="my-0 fw-normal text-dark">UX 3 Task complete.</h6>
-                                    <small class="text-muted mb-0">Dummy text of the printing.</small>
-                                </div><!--end media-body-->
-                            </div><!--end media-->
-                        </a><!--end-item-->
-                        <!-- item-->
-                        <a href="#" class="dropdown-item py-3">
-                            <small class="float-end text-muted ps-2">1 hr ago</small>
-                            <div class="media">
-                                <div class="avatar-md bg-soft-primary">
-                                    <img src="assets/images/users/user-5.jpg" alt="" class="thumb-sm rounded-circle">
-                                </div>
-                                <div class="media-body align-self-center ms-2 text-truncate">
-                                    <h6 class="my-0 fw-normal text-dark">Your order is placed</h6>
-                                    <small class="text-muted mb-0">It is a long established fact that a reader.</small>
-                                </div><!--end media-body-->
-                            </div><!--end media-->
-                        </a><!--end-item-->
-                        <!-- item-->
-                        <a href="#" class="dropdown-item py-3">
-                            <small class="float-end text-muted ps-2">2 hrs ago</small>
-                            <div class="media">
-                                <div class="avatar-md bg-soft-primary">
-                                    <i data-feather="check-circle" class="align-self-center icon-xs"></i>
-                                </div>
-                                <div class="media-body align-self-center ms-2 text-truncate">
-                                    <h6 class="my-0 fw-normal text-dark">Payment Successfull</h6>
-                                    <small class="text-muted mb-0">Dummy text of the printing.</small>
-                                </div><!--end media-body-->
-                            </div><!--end media-->
-                        </a><!--end-item-->
-                    </div>
-                    <!-- All-->
-                    <a href="javascript:void(0);" class="dropdown-item text-center text-primary">
-                        View all <i class="fi-arrow-right"></i>
-                    </a>
-                </div>
-            </li>
-
-            <li class="dropdown">
-                <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-bs-toggle="dropdown" href="#" role="button"
-                    aria-haspopup="false" aria-expanded="false">
-                    <span class="ms-1 nav-user-name hidden-sm">Nick</span>
-                    <img src="assets/images/users/user-5.jpg" alt="profile-user" class="rounded-circle thumb-xs" />
-                </a>
-                <div class="dropdown-menu dropdown-menu-end">
-                    <a class="dropdown-item" href="pages-profile.html"><i data-feather="user" class="align-self-center icon-xs icon-dual me-1"></i> Profile</a>
-                    <a class="dropdown-item" href="apps-contact-list.html"><i data-feather="users" class="align-self-center icon-xs icon-dual me-1"></i> Contacts</a>
-                    <div class="dropdown-divider mb-0"></div>
-                    <a class="dropdown-item" href="{{ route('logout') }}"><i data-feather="power" class="align-self-center icon-xs icon-dual me-1"></i> Logout</a>
-                </div>
-            </li>
-        </ul><!--end topbar-nav-->
+                <ul class="navbar-nav header-right">
+                    <li class="nav-item border-0">
+                        <a class="btn btn-primary create-event-btn" href="#" target="_blank">Create Event</a>
+                    </li>
+                    <li class="nav-item dropdown notification_dropdown">
+                        <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="mdi mdi-bell"></i>
+                            <span class="badge badge-primary">3</span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <div class="dropdown-header">
+                                <h5 class="notification_title">Notifications</h5>
+                            </div>
+                            <ul class="list-unstyled">
+                                <li class="media dropdown-item">
+                                    <span class="text-primary"><i class="mdi mdi-chart-areaspline mr-3"></i></span>
+                                    <div class="media-body">
+                                        <a href="#">
+                                            <div class="d-flex justify-content-between">
+                                                <h5>New order has been received</h5>
+                                            </div>
+                                            <p class="m-0">2 hours ago</p>
+                                        </a>
+                                        <i class="fa fa-angle-right"></i>
+                                    </div>
+                                </li>
+                                <li class="media dropdown-item">
+                                    <span class="text-success"><i class="mdi mdi-chart-pie mr-3"></i></span>
+                                    <div class="media-body">
+                                        <a href="#">
+                                            <div class="d-flex justify-content-between">
+                                                <h5>New customer is registered</h5>
+                                            </div>
+                                            <p class="m-0">3 hours ago</p>
+                                        </a>
+                                        <i class="fa fa-angle-right"></i>
+                                    </div>
+                                </li>
+                                <li class="media dropdown-item">
+                                    <span class="text-warning"><i class="mdi mdi-file-document mr-3"></i></span>
+                                    <div class="media-body">
+                                        <a href="#">
+                                            <div class="d-flex justify-content-between">
+                                                <h5>New file has been uploaded</h5>
+                                            </div>
+                                            <p class="m-0">3 hours ago</p>
+                                        </a>
+                                        <i class="fa fa-angle-right"></i>
+                                    </div>
+                                </li>
+                            </ul>
+                            <a class="all-notification" href="#">All Notifications</a>
+                        </div>
+                    </li>
+                    <li class="nav-item dropdown header-profile">
+                        <a class="nav-link" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <img src="images/users/2.jpg" alt="">
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <div class="dropdown-profile-header">
+                                <img src="images/users/2.jpg" alt="">
+                                <span class="avatar-name ml-2">John Doe</span>
+                            </div>
+                            <a href="app-profile.html" class="dropdown-item">
+                                <i class="mdi mdi-account"></i>
+                                <span>Profile</span>
+                            </a>
+                            <a href="home-create-ticket.html" class="dropdown-item">
+                                <i class="mdi mdi-ticket"></i>
+                                <span>Create Ticket</span>
+                            </a>
+                            <a href="app-calender.html" class="dropdown-item">
+                                <i class="mdi mdi-calendar-check"></i>
+                                <span>Calendar</span>
+                            </a>
+                            <a href="email-inbox.html" class="dropdown-item">
+                                <i class="mdi mdi-email"></i>
+                                <span>Inbox</span>
+                            </a>
+                            <a href="page-login.html" class="dropdown-item">
+                                <i class="mdi mdi-power"></i>
+                                <span>{{ route('logout') }}</span>
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</div>
