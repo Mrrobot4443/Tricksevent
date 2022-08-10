9d67c6cc14e9ab6dceb5e675655fe65a97d90b2d
Yassine Bess <yazo-yazo@hotmail.com>
2022-08-10 21:37:58 +0100

    up

--- app/Models/Villes.php
+++ app/Models/Villes.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class adresses extends Model
+class Villes extends Model
 {
     use HasFactory;
 }
